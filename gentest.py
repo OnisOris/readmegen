9eafd7a5ed2a8fd8fa545707fc33140086499b1d
Michael <onisoris@yandex.ru>
2025-01-27 12:38:09 +0300

    Загрузка файла для генерации с самого модуля

--- gentest.py
+++ gentest.py
@@ -2,8 +2,8 @@ from readmegen import ReadmeGenerator
 
 if __name__ == "__main__":
     dirs = ["img", "description"]
-    lists = [[['./readmegen/readmegen.py'],
+    lists = [[['./readmegen/readmegen.py', './readmegen/functions.py'],
                         './img/graph.png']]
     rgen = ReadmeGenerator(lists)
-    rgen.generate('./test/')
+    rgen.generate('./readmegen/')
      
